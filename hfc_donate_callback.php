0d17c48ed6ee48ca20e1af31be20d66c6884d480
Frank Riccobono <F.Riccobono@gmail.com>
2014-04-27 19:51:43 -0400

    fixed callback code

--- hfc_donate_callback.php
+++ hfc_donate_callback.php
@@ -1,17 +1,17 @@
 <?php
 
-add_action( 'init', 'hfc_donate_init_internal' );
 function hfc_donate_init_internal() {
-    add_rewrite_rule( 'donate/ipn/?$', 'index.php?hfc_donate_api=1', 'top' );
+   add_rewrite_rule('^ipn/?','index.php?hfc_donate_api=1','top');
 }
+add_action( 'init', 'hfc_donate_init_internal' );
 
-add_filter( 'query_vars', 'hfc_donate_query_vars' );
 function hfc_donate_query_vars( $query_vars ) {
     $query_vars[] = 'hfc_donate_api';
     return $query_vars;
 }
 
-add_action( 'parse_request', 'hfc_donate_parse_request' );
+add_filter( 'query_vars', 'hfc_donate_query_vars' );
+
 function hfc_donate_parse_request( &$wp )
 {
     if ( array_key_exists( 'hfc_donate_api', $wp->query_vars ) ) {
@@ -20,4 +20,5 @@ function hfc_donate_parse_request( &$wp )
     }
     return;
 }
+add_action( 'parse_request', 'hfc_donate_parse_request' );
 ?>
